d1c88a8399b487400c66f0b04c54aeb8e7340fb3
shark802 <crimsonlove12345@gmail.com>
2025-11-17 13:34:08 +0800

    commit

--- app.py
+++ app.py
@@ -283,7 +283,7 @@ def clean_date_string(date_str):
         # Full month names
         'january': '01', 'february': '02', 'march': '03', 'april': '04', 
         'may': '05', 'june': '06', 'july': '07', 'august': '08',
-        'september': '09', 'october': '10', 'november': '11', 'december': '12',
+        'september': '09', 'october': '10', 'november': '11', 'december': '12','sept': '09',
         # 3-letter abbreviations
         'jan': '01', 'feb': '02', 'mar': '03', 'apr': '04',
         'jun': '06', 'jul': '07', 'aug': '08', 'sep': '09',
